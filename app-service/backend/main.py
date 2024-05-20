3df3260582670653cb95f4a180ab30ed9d68ada0
unknown <uzairabid222@gmail.com>
2024-05-21 02:45:21 +0500

    multiple pages handling

--- app-service/backend/main.py
+++ app-service/backend/main.py
@@ -11,7 +11,10 @@ import os
 from flask_cors import CORS
 from collections import defaultdict
 from dotenv import load_dotenv
+import time
+import PyPDF2
 
+from flask import request
 
 
 load_dotenv()
@@ -19,12 +22,30 @@ aws_region_name = os.getenv("aws_region_name")
 aws_access_key_id = os.getenv("aws_access_key_id")
 aws_secret_access_key = os.getenv("aws_secret_access_key")
 
+s3_access_key = os.getenv('s3_access_key')
+s3_secret_key = os.getenv('s3_secret_key')
+s3_bucket_name = os.getenv('s3_bucket_name')
+s3_bucket_region = os.getenv('s3_bucket_region')
+
 app = Flask(__name__)
 
 CORS(app)
 
 
-from flask import request
+def get_pdf_num_pages(file_bytes):
+    try:
+        pdf_reader = PyPDF2.PdfFileReader(file_bytes)
+        num_pages = pdf_reader.numPages
+        return num_pages
+    except:
+        return 0 
+
+def upload_pdf_to_s3_2(pdf_content, pdf_file_name):
+    s3_client = boto3.client('s3', aws_access_key_id=s3_access_key, aws_secret_access_key=s3_secret_key)
+    s3_key = pdf_file_name
+    s3_client.put_object(Body=pdf_content, Bucket=s3_bucket_name, Key=s3_key, ContentType='application/pdf')
+    s3_url = f'https://{s3_bucket_name}.s3.{aws_region_name}.amazonaws.com/{s3_key}'
+    return s3_url
 
 @app.route("/textract/queries", methods=["POST"])
 def use_textract_queries():
@@ -37,25 +58,44 @@ def use_textract_queries():
         if file.filename == '':
             return {"error": "No file selected!"}, 400
 
-        file_bytes = file.read()
+        num_pages = get_pdf_num_pages(file)
+        print(num_pages)
+        if num_pages == 0:
+            return {"error": "Invalid PDF or unable to read the file"}, 400
         
         query_list = []
-        
+        client = boto3.client('textract', region_name=aws_region_name, aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
+
+
         for key, value in request.form.items():
             query_list.append({
                 "Text": value,
-                "Alias": value
+                "Alias": value,
+                "Pages": [str(num_pages)]
                 })
       
-        client = boto3.client('textract', region_name=aws_region_name, aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
         
-        response = client.analyze_document(
-            Document={'Bytes': file_bytes},
+        response = client.start_document_analysis(
+            DocumentLocation={'S3Object': {'Bucket': s3_bucket_name, 'Name': file.filename}},
             FeatureTypes=["QUERIES"],
             QueriesConfig={
                 "Queries": query_list
             }
         )
+
+        job_id = response['JobId']
+        status = None
+
+        print(job_id)
+
+        while status not in ['SUCCEEDED', 'FAILED']:
+            response = client.get_document_analysis(JobId=job_id)
+            status = response['JobStatus']
+            if status == 'SUCCEEDED':
+                break
+            elif status == 'FAILED':
+                return {"error": "Document analysis failed"}, 500
+            time.sleep(2)
 
         data_list = {}
 
@@ -70,7 +110,7 @@ def use_textract_queries():
 
 
 
-@app.route("/textract/tables", methods=["POST"])
+@app.route("/textract/ocr", methods=["POST"])
 def use_textract_tables():
     try:
         if 'file' not in request.files:
@@ -83,23 +123,37 @@ def use_textract_tables():
 
         file_bytes = file.read()
 
+        s3_url = upload_pdf_to_s3_2(file_bytes, file.filename)
+
         client = boto3.client('textract', region_name=aws_region_name, aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
-        
-        response = client.analyze_document(
-            Document={'Bytes': file_bytes},
-            FeatureTypes=["TABLES"],
+        response = client.start_document_analysis(
+            DocumentLocation={'S3Object': {'Bucket': s3_bucket_name, 'Name': file.filename}},
+            FeatureTypes=["TABLES"]
         )
 
-        data_list = []
+        job_id = response['JobId']
+        status = None
+        while status not in ['SUCCEEDED', 'FAILED']:
+            response = client.get_document_analysis(JobId=job_id)
+            status = response['JobStatus']
+            if status == 'SUCCEEDED':
+                break
+            elif status == 'FAILED':
+                return {"error": "Document analysis failed"}, 500
+            time.sleep(2)
 
-        for idx, item in enumerate(response["Blocks"]):
-            if item["BlockType"] == "LINE":
-                data_list.append(item['Text'])
 
-        return {"text_data": data_list}, 200
+        text_data = []
+        for block in response['Blocks']:
+            if block['BlockType'] == 'WORD':
+                text_data.append(block['Text'])
+
+        return {"text_data": text_data, "s3_url": s3_url}, 200
+
 
     except Exception as e:
         return {"error": str(e)}, 500
+
 
 
 
@@ -115,15 +169,40 @@ def use_textract_forms():
             return {"error": "No file selected!"}, 400
 
         file_bytes = file.read()
+        s3_url = upload_pdf_to_s3_2(file_bytes, file.filename)
 
+  
         client = boto3.client('textract', region_name=aws_region_name, aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
-        
-        response = client.analyze_document(
-            Document={'Bytes': file_bytes},
-            FeatureTypes=["FORMS"],
+        response = client.start_document_analysis(
+            DocumentLocation={'S3Object': {'Bucket': s3_bucket_name, 'Name': file.filename}},
+            FeatureTypes=["FORMS"]
         )
 
-        blocks = response["Blocks"]
+        job_id = response['JobId']
+
+        status = None
+        while status not in ['SUCCEEDED', 'FAILED']:
+            response = client.get_document_analysis(JobId=job_id)
+            status = response['JobStatus']
+            if status == 'SUCCEEDED':
+                break
+            elif status == 'FAILED':
+                return {"error": "Document analysis failed"}, 500
+            time.sleep(2)
+
+
+        blocks = []
+        next_token = None
+        while True:
+            if next_token:
+                response = client.get_document_analysis(JobId=job_id, NextToken=next_token)
+            else:
+                response = client.get_document_analysis(JobId=job_id)
+            
+            blocks.extend(response["Blocks"])
+            next_token = response.get("NextToken")
+            if not next_token:
+                break
 
         key_map = {}
         value_map = {}
@@ -144,19 +223,17 @@ def use_textract_forms():
             val = get_text(value_block, block_map)
             kvs[key].append(val)
 
-        return {"form_data": kvs}, 200
+        return {"form_data": dict(kvs), "s3_url": s3_url}, 200
 
     except Exception as e:
         return {"error": str(e)}, 500
-
 
 def find_value_block(key_block, value_map):
     for relationship in key_block['Relationships']:
         if relationship['Type'] == 'VALUE':
             for value_id in relationship['Ids']:
-                value_block = value_map[value_id]
-    return value_block
-
+                return value_map.get(value_id, None)
+    return None
 
 def get_text(result, blocks_map):
     text = ''
@@ -167,10 +244,9 @@ def get_text(result, blocks_map):
                     word = blocks_map[child_id]
                     if word['BlockType'] == 'WORD':
                         text += word['Text'] + ' '
-                    if word['BlockType'] == 'SELECTION_ELEMENT':
+                    elif word['BlockType'] == 'SELECTION_ELEMENT':
                         if word['SelectionStatus'] == 'SELECTED':
                             text += 'X '
-
     return text
             
 
